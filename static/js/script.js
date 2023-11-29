a0a0cd3c0aeb247d48811158d4e37193fc262848
Dylan Redmond <dylan.redmond@workhuman.com>
2023-11-29 21:03:03 +0000

    Adding develop app for different look

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,10 @@
-// script.js
-window.addEventListener('DOMContentLoaded', (event) => {
-    let lastActiveMenuItem = null;
+function toggleSubMenu(submenuId) {
+    var submenus = document.getElementsByClassName('submenu');
+    for (var i = 0; i < submenus.length; i++) {
+        submenus[i].style.display = 'none'; // Hide all submenus
+    }
 
-    window.showSubMenu = function (submenuId) {
-        // Hide any previously shown submenu
-        if (lastActiveMenuItem && lastActiveMenuItem !== submenuId) {
-            document.getElementById(lastActiveMenuItem).style.display = 'none';
-            document.querySelector(`li.active`).classList.remove('active');
-        }
-
-        // Toggle the display of the submenu
-        const submenu = document.getElementById(submenuId);
-        if (submenu.style.display === 'none') {
-            submenu.style.display = 'block';
-            document.querySelector(`li[onclick="showSubMenu('${submenuId}');"]`).classList.add('active');
-        } else {
-            submenu.style.display = 'none';
-            document.querySelector(`li[onclick="showSubMenu('${submenuId}');"]`).classList.remove('active');
-        }
-
-        lastActiveMenuItem = submenuId; // Keep track of the last active menu item
-    };
-
-    window.loadContent = function (contentId) {
-        // Load content for the selected submenu item
-        // You can use AJAX here or just simple content swapping
-        console.log('Load content for:', contentId);
-        // Implement content loading logic...
-    };
-});
-
+    var activeSubMenu = document.getElementById(submenuId);
+    var isDisplayed = activeSubMenu.style.display === 'block';
+    activeSubMenu.style.display = isDisplayed ? 'none' : 'block'; // Toggle display of the active submenu
+}
